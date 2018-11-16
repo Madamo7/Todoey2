580bbcd38f61fb3e6a3b7ee2953ba8221919649c
madamo <madamo@poczta.onet.pl>
2018-11-16 05:36:11 +0100

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by Mikolaj Adamowicz on 16/11/2018.
+//  Copyright © 2018 Mikolaj Adamowicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
